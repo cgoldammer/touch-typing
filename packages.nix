0702e6c4f6462994ffe89d65810854a31c451375
cgoldammer <goldammer.christian@gmail.com>
2016-08-15 01:25:46 +0000

    Added default.nix

---
+++ packages.nix
@@ -0,0 +1,24 @@
+{ haskellPackages, platform }:
+
+with haskellPackages;
+
+[
+  ##############################################################################
+  # Add general packages here                                                  #
+  ##############################################################################
+  reflex
+  reflex-dom
+  reflex-todomvc
+  random
+
+] ++ (if platform == "ghcjs" then [
+  ##############################################################################
+  # Add ghcjs-only packages here                                               #
+  ##############################################################################
+
+] else []) ++ (if platform == "ghc" then [
+  ##############################################################################
+  # Add ghc-only packages here                                                 #
+  ##############################################################################
+
+] else []) ++ builtins.concatLists (map (x: x.override { mkDerivation = drv: drv.buildDepends; }) [ reflex reflex-dom reflex-todomvc ])
